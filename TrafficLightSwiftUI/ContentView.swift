fe2e5e8c187c7790a00a21383335dc71b4fa3a2a
Denis Denisov <denis.denisov8701@gmail.com>
2024-04-16 23:57:10 +0200

    logic for switch light

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
     
+    @State private var currentLight = CurrentLight.red
     private let lightIsOn = 1.0
     private let lightIsOff = 0.3
     
@@ -17,21 +21,47 @@ struct ContentView: View {
             VStack {
                 Circle()
                     .foregroundStyle(.red)
+                    .opacity(currentLight == .red 
+                             ? lightIsOn
+                             : lightIsOff)
                 Circle()
                     .foregroundStyle(.yellow)
+                    .opacity(currentLight == .yellow 
+                             ? lightIsOn
+                             : lightIsOff)
                 Circle()
                     .foregroundStyle(.green)
+                    .opacity(currentLight == .green 
+                             ? lightIsOn
+                             : lightIsOff)
             }
             .frame(width: 200, height: 500)
             
             Spacer()
             
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
-            })
+            Button(action: buttonAction) {
+                Text("START")
+                    .font(.largeTitle)
+                    .padding()
+            }
+            .foregroundStyle(.white)
+            .background(.blue)
+            .clipShape(.buttonBorder)
         }
         .padding()
     }
+    
+    func buttonAction() {
+        switch currentLight {
+        case .red:
+            currentLight = .yellow
+        case .yellow:
+            currentLight = .green
+        case .green:
+            currentLight = .red
+        }
+    }
+
 }
 
 #Preview {
