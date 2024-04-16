43f86c9eda57f810623e46ef8e26a5b9d4d03227
Denis Denisov <denis.denisov8701@gmail.com>
2024-04-16 21:40:23 +0200

    Initial Commit

---
+++ TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Denis Denisov on 16/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
