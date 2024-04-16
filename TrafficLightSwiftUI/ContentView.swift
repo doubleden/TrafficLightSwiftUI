afbc6a8be9ef9647322cbcb5134d983155114a2a
Denis Denisov <denis.denisov8701@gmail.com>
2024-04-16 23:17:38 +0200

    UISetup

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private let lightIsOn = 1.0
+    private let lightIsOff = 0.3
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                Circle()
+                    .foregroundStyle(.red)
+                Circle()
+                    .foregroundStyle(.yellow)
+                Circle()
+                    .foregroundStyle(.green)
+            }
+            .frame(width: 200, height: 500)
+            
+            Spacer()
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+            })
         }
         .padding()
     }
